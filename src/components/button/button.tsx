836ecc3899b200e9c3d86f168c9283288700b939
anonymousrecords <a.Records2022@gmail.com>
2024-04-06 23:03:23 +0900

    feat : Button 컴포넌트 구현

---
+++ src/components/button/button.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { css } from "@emotion/react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variants: "primary" | "secondary";
+  disabled?: boolean;
+  onClick?: () => void;
+}
+
+const base = css({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  fontSize: "17px",
+  fontWeight: "600",
+  borderRadius: "8px",
+  width: "100%",
+  height: "52px",
+  boxShadow: "0px 0px 16px 2px rgba(0, 0, 0, 0.02)",
+  "&:disabled": {
+    backgroundColor: "#D9D9D9",
+    color: "white",
+    cursor: "default",
+  },
+});
+
+const variantsCss = {
+  primary: css({
+    backgroundColor: "#1E212B",
+    color: "white",
+  }),
+  secondary: css({
+    backgroundColor: "#009436",
+    color: "white",
+  }),
+  territory: css({
+    backgroundColor: "#FFFFFF",
+    color: "#1E212B",
+  }),
+};
+
+const Button: React.FC<ButtonProps> = ({
+  variants,
+  type = "button",
+  disabled,
+  children,
+  ...props
+}: ButtonProps) => {
+  const buttonStyle = [base, variantsCss[variants]];
+
+  return (
+    <button type={type} css={buttonStyle} disabled={disabled} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
