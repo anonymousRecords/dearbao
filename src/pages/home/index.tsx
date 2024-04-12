5b888180ba75f82147a7b6b9f358353b26fb5991
anonymousrecords <a.Records2022@gmail.com>
2024-04-12 21:47:05 +0900

    feat : 홈 페이지 미션 관련 부분 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import { Drawer } from "@/components/drawer";
 import { Header } from "@/components/header";
 import { IconButton } from "@/components/icon-button";
-import { useNickname } from "@/hook/useNickname";
 import useCurrentTime from "@/hook/useCurrentTime";
+import useMission from "@/hook/useMission";
+import { useRouter } from "next/router";
 
 const Home = () => {
+  const router = useRouter();
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   const openDrawer = () => {
@@ -16,11 +18,16 @@ const Home = () => {
     setIsDrawerOpen(false);
   };
 
-  // 닉네임 데이터 가져오기
-  const { nickname } = useNickname();
-
   // 현재 시간 가져오기 (시:분:초)
   const currentTime = useCurrentTime(1000);
+
+  const { missionData } = useMission();
+  const nickname = missionData?.nickname;
+  const messageOpenTime = missionData?.messageOpenTime;
+  const missionSuccess = missionData?.missionSuccess;
+  const remainingTime = missionData?.remainingTime;
+  const message = missionData?.message;
+
   return (
     <>
       <Header
@@ -32,7 +39,20 @@ const Home = () => {
       />
       <p>{nickname}</p>
       <p>{currentTime}</p>
-      <button>데일리 미션 버튼</button>
+      {messageOpenTime ? (
+        <>
+          <p>미션 오픈 시간입니다.</p>
+          <p>{remainingTime}</p>
+        </>
+      ) : (
+        <>
+          <p>미션 오픈 시간이 아닙니다!</p>
+          <p>{String(missionSuccess)}</p>
+          <p>{message}</p>
+          <button onClick={() => router.push("/daily")}>데일리 미션</button>
+        </>
+      )}
+
       <Drawer isOpen={isDrawerOpen} onClose={closeDrawer} />
     </>
   );
