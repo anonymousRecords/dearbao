c7ce2231461560e1bd7b6070b65bb1b8bb8161bf
anonymousrecords <a.Records2022@gmail.com>
2024-04-12 19:53:58 +0900

    feat : 닉네임 & 실시간 시간 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Drawer } from "@/components/drawer";
 import { Header } from "@/components/header";
 import { IconButton } from "@/components/icon-button";
+import { useNickname } from "@/hook/useNickname";
+import useCurrentTime from "@/hook/useCurrentTime";
 
 const Home = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
@@ -13,6 +15,12 @@ const Home = () => {
   const closeDrawer = () => {
     setIsDrawerOpen(false);
   };
+
+  // 닉네임 데이터 가져오기
+  const { nickname } = useNickname();
+
+  // 현재 시간 가져오기 (시:분:초)
+  const currentTime = useCurrentTime(1000);
   return (
     <>
       <Header
@@ -22,8 +30,9 @@ const Home = () => {
           <IconButton imgSrc="/svg/menu.svg" width={25} onClick={openDrawer} />
         }
       />
+      <p>{nickname}</p>
+      <p>{currentTime}</p>
       <button>데일리 미션 버튼</button>
-      <p>홈 화면</p>
       <Drawer isOpen={isDrawerOpen} onClose={closeDrawer} />
     </>
   );
