5ac07d423ee162ef5059571d4d692f01a7974d17
anonymousrecords <a.Records2022@gmail.com>
2024-04-13 22:17:01 +0900

    feat : 리프레시 토큰 발급 로직 추가

--- src/pages/redirect/index.tsx
+++ src/pages/redirect/index.tsx
@@ -1,43 +1,78 @@
-import { setCookie } from "cookies-next";
-import { useEffect } from "react";
+import { setCookie, getCookie } from "cookies-next";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { postLogin } from "../../api/postLogin";
+import { postRefreshToken } from "../../api/postRefreshToken";
 
 const Redirect = () => {
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const { query } = router;
     const code = query.code as string;
 
-    const getAuth = async (code: string) => {
-      const data = await postLogin(code);
-      const accessToken = data.accessToken;
-      const refreshToken = data.refreshToken;
-      const initProfile = data.initProfile;
+    const handleLogin = async (code: string) => {
+      try {
+        const data = await postLogin(code);
+        const accessToken = data.accessToken;
+        const refreshToken = data.refreshToken;
+        const initProfile = data.initProfile;
 
-      if (data) {
-        setCookie("accessToken", accessToken, {
-          maxAge: 60 * 3000,
-        });
-        setCookie("refreshToken", refreshToken, {
-          maxAge: 60 * 60 * 24 * 30,
-        });
-      }
+        if (data) {
+          setCookie("accessToken", accessToken, {
+            maxAge: 60 * 3000,
+          });
+          setCookie("refreshToken", refreshToken, {
+            maxAge: 60 * 60 * 24 * 30,
+          });
+        }
 
-      if (!initProfile) {
-        router.push("/nickname");
-        return;
+        if (!initProfile) {
+          router.push("/nickname");
+        } else {
+          router.push("/home");
+        }
+      } catch (error) {
+        console.error("Error while logging in:", error);
       }
-      router.push("/home");
+    };
+
+    const handleRefreshToken = async () => {
+      try {
+        const accessToken = getCookie("accessToken");
+        const refreshToken = getCookie("refreshToken");
+
+        if (!accessToken || !refreshToken) {
+          router.push("/onboarding");
+          return;
+        }
+
+        const response = await postRefreshToken();
+
+        if (response && response.accessToken) {
+          setCookie("accessToken", response.accessToken, {
+            maxAge: 60 * 3000,
+          });
+          setLoading(false);
+        }
+      } catch (error) {
+        console.error("리프레시 토큰 발급 에러 발생! :", error);
+        router.push("/onboarding");
+      }
     };
 
     if (!code) {
       return;
     }
 
-    getAuth(code);
+    handleLogin(code);
+    handleRefreshToken();
   }, [router]);
+
+  if (loading) {
+    return null;
+  }
 
   return null;
 };
