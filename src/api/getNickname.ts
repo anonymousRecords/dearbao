46c20f8b11880e762787d85fd990f7f27933fb3a
anonymousrecords <a.Records2022@gmail.com>
2024-04-17 00:12:49 +0900

    feat : log 제거

--- src/api/getNickname.ts
+++ src/api/getNickname.ts
@@ -5,7 +5,5 @@ export const getNickname = async () => {
   const data = await baseAxios.get<APIResponse<Nickname>>(
     `api/v1/member/nickname`
   );
-  
-    console.log("getNickname.ts", data.data);
   return data.data;
 };
