7c2e84192d64c4e95aa6dd5f8c9ea31ecb69a6ab
anonymousrecords <a.Records2022@gmail.com>
2024-05-05 19:43:50 +0900

    feat : 튜토리얼 페이지 수정 (이미지 변경)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,8 +21,8 @@ export default function Tutorial() {
   const tutorialSlides = [
     {
       id: 0,
-      title: "도착한 쪽지",
-      imgSrc: `/img/tutorial/tutorial-1.jpeg`,
+      title: "디어바오로부터 도착한 쪽지",
+      imgSrc: `/img/tutorial/tutorial_1.png`,
       description: `매일 오전 9시, 푸바오가 질문이 담긴 쪽지를 보내요!\n
       쪽지를 클릭해 질문을 확인해보세요\n
       오후 8시 59분까지 쪽지에 대한 편지를 보낼 수 있답니다`,
@@ -30,23 +30,14 @@ export default function Tutorial() {
     {
       id: 1,
       title: "푸바오의 답장",
-      imgSrc: `/img/tutorial/tutorial-${1}.jpeg`,
+      imgSrc: `/img/tutorial/tutorial_2.png`,
       description: `편지를 보냈다면 매일 오후 9시, 답장을 받을 수 있어요\n
       푸바오가 내 이름을 불러주며 보내는 답장을 받아보세요`,
     },
     {
       id: 2,
-      title: "푸바오를 향한 나의 마음",
-      imgSrc: `/img/tutorial/tutorial-1.jpeg`,
-      description: `
-      편지 하나를 쓸 때마다 마음의 온도가 조금씩 올라가요\n
-      매일매일 편지를 써 36.5℃를 채워보세요
-      `,
-    },
-    {
-      id: 3,
       title: "내 앨범 속 푸바오의 답장",
-      imgSrc: `/img/tutorial/tutorial-1.jpeg`,
+      imgSrc: `/img/tutorial/tutorial_3.png`,
       description: `
       받은 답장을 캘린더에 모아볼 수 있어요\n
       편지지에 담긴 답장을 다운로드 해서 간직할 수도 있답니다!
@@ -81,6 +72,7 @@ export default function Tutorial() {
               alignItems: "center",
               width: "100%",
               gap: "10px",
+              padding: "0px 20px",
             }}
           >
             <Image
@@ -91,9 +83,10 @@ export default function Tutorial() {
             />
             <h2
               style={{
-                fontSize: "20px",
+                fontSize: "24px",
                 fontWeight: 700,
-                margin: "25px 0px 5px 0px",
+                margin: "25px 0px 7px 0px",
+                fontFamily: "EF_jejudoldam",
               }}
             >
               {slide.title}
@@ -106,32 +99,23 @@ export default function Tutorial() {
             >
               {slide.description}
             </p>
-            {slide.id === 3 ? (
+            {slide.id === 2 ? (
               <Button
                 variants="primary"
                 onClick={() => router.push("/onboarding")}
                 style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  fontSize: "17px",
-                  fontWeight: "600",
-                  borderRadius: "16px",
-                  width: "80%",
-                  height: "52px",
-                  boxShadow: "0px 0px 16px 2px rgba(0, 0, 0, 0.02)",
-                  color: "white",
-                  backgroundColor: "#1E212B",
                   marginTop: "20px",
                 }}
               >
                 시작하기
               </Button>
             ) : (
-              <div style={{
-                width: "80%",
-                height: "52px",
-              }}></div>
+              <div
+                style={{
+                  width: "80%",
+                  height: "52px",
+                }}
+              ></div>
             )}
           </div>
         </SwiperSlide>
